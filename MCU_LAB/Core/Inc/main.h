719a9354571131e653bb9c592f0c3a11bec1553a
VAIO\Minhq <minhquan482212@gmail.com>
2023-10-09 17:19:34 +0700

    Init Ex6

--- MCU_LAB/Core/Inc/main.h
+++ MCU_LAB/Core/Inc/main.h
@@ -58,6 +58,30 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define PA4_Pin GPIO_PIN_4
+#define PA4_GPIO_Port GPIOA
+#define PA5_Pin GPIO_PIN_5
+#define PA5_GPIO_Port GPIOA
+#define PA6_Pin GPIO_PIN_6
+#define PA6_GPIO_Port GPIOA
+#define PA7_Pin GPIO_PIN_7
+#define PA7_GPIO_Port GPIOA
+#define PA8_Pin GPIO_PIN_8
+#define PA8_GPIO_Port GPIOA
+#define PA9_Pin GPIO_PIN_9
+#define PA9_GPIO_Port GPIOA
+#define PA10_Pin GPIO_PIN_10
+#define PA10_GPIO_Port GPIOA
+#define PA11_Pin GPIO_PIN_11
+#define PA11_GPIO_Port GPIOA
+#define PA12_Pin GPIO_PIN_12
+#define PA12_GPIO_Port GPIOA
+#define PA13_Pin GPIO_PIN_13
+#define PA13_GPIO_Port GPIOA
+#define PA14_Pin GPIO_PIN_14
+#define PA14_GPIO_Port GPIOA
+#define PA15_Pin GPIO_PIN_15
+#define PA15_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
--- MCU_LAB/Core/Src/main.c
+++ MCU_LAB/Core/Src/main.c
@@ -47,6 +47,7 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -83,17 +84,23 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  uint16_t LED [12] = { GPIO_PIN_4 , GPIO_PIN_5 , GPIO_PIN_6 , GPIO_PIN_7 ,
+  GPIO_PIN_8 , GPIO_PIN_9 , GPIO_PIN_10 , GPIO_PIN_11 , GPIO_PIN_12 ,
+  GPIO_PIN_13 , GPIO_PIN_14 , GPIO_PIN_15 };
+  int i = 0;
   while (1)
   {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
+	  if (i >= 12) i = 0;
+	  	  HAL_GPIO_TogglePin (GPIOA , LED [i]);
+	  	  i++;
+	  	  HAL_Delay (1000) ;
   }
   /* USER CODE END 3 */
 }
@@ -131,6 +138,36 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, PA4_Pin|PA5_Pin|PA6_Pin|PA7_Pin
+                          |PA8_Pin|PA9_Pin|PA10_Pin|PA11_Pin
+                          |PA12_Pin|PA13_Pin|PA14_Pin|PA15_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PA4_Pin PA5_Pin PA6_Pin PA7_Pin
+                           PA8_Pin PA9_Pin PA10_Pin PA11_Pin
+                           PA12_Pin PA13_Pin PA14_Pin PA15_Pin */
+  GPIO_InitStruct.Pin = PA4_Pin|PA5_Pin|PA6_Pin|PA7_Pin
+                          |PA8_Pin|PA9_Pin|PA10_Pin|PA11_Pin
+                          |PA12_Pin|PA13_Pin|PA14_Pin|PA15_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
